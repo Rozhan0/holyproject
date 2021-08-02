0d4b851316874085f72b2b0d87fb00b3743b1622
k3y_1 <keyvan_navyek@protonmail.com>
2021-08-02 05:23:57 +0430

    param list data structure changed to linked list, new functions added(push_param, free_req_data)

--- parse/parse.c
+++ parse/parse.c
@@ -1,5 +1,7 @@
 #include "parse.h"
 
+#define DEBUG 1
+
 char * create_response(char * msg) {
     char * output = calloc(strlen(msg) + 200, sizeof(char));
     sprintf(output,
@@ -9,25 +11,38 @@ char * create_response(char * msg) {
     return output;
 }
 
+int push_param(param * pr, req_data * data) {
+    /* this function pushes a param structure to linked list
+    inside req_data structure */
+    
+    if(pr != 0 && data != 0) {
+        if(data->param_list_top)
+            data->param_list_top->next = pr;
+        else
+            data->param_list_head = pr;
+        data->param_list_top = pr;
+        return 1;
+    } else return 0;
+}
+
 int parse_query(char * query, req_data * data) {
     /* this function parses a query string into an
     array of parameters which every one of them holds
     a key and a value */
 
     char * parameter, * rest = query;
-    data->length = 0;
-    data->params = calloc(0, sizeof(param));
     while(parameter = strtok_r(rest, "&", &rest)) {
-        data->length++;
-        data->params = realloc(data->params, data->length * sizeof(param));
-        param * pr = data->params + data->length - 1;
+        param * pr = calloc(1, sizeof(param));
         char * equal_mark = strchr(parameter, '=');
         if(equal_mark == 0) {
-            puts("parse.c->parse_get_request-> Broken Query.");
+            DEBUG && puts("parse.c->parse_query-> Broken Query.");
             return 0;
         }
         *equal_mark = ' ';
         sscanf(parameter, "%s %s", pr->key, pr->value);
+        if(!push_param(pr, data)) {
+            DEBUG && puts("parse.c->parse_query-> null pointer.");
+        }
     }
     return 1;
 }
@@ -36,11 +51,11 @@ int parse_get_request(const char * request, req_data * data) {
     /* this function parses a request to path and query.
     if query exists, it will be passed to parse_query */
 
-    char path[129] = {0};
+    char path[257] = {0};
     char * querstion_mark = strchr(request, '?');
     if(querstion_mark) {
         *querstion_mark = ' ';
-        char query[129] = {0};
+        char query[257] = {0};
         sscanf(request, "GET %s %s ", path, query);
         if(parse_query(query, data) == 0) return 0;
     } else {
@@ -49,4 +64,18 @@ int parse_get_request(const char * request, req_data * data) {
     data->path = calloc(strlen(path) + 1, sizeof(char));
     strcpy(data->path, path);
     return 1;
+}
+
+void free_req_data(req_data * data) {
+    /* this function, does free memory allocated for
+    the linked list and path in req_data structure */
+
+    param * pr1 = data->param_list_head;
+    param * pr2;
+    do {
+        pr2 = pr1->next;
+        free(pr1);
+    } while(pr1 = pr2);
+
+    free(data->path);
 }
--- parse/parse.h
+++ parse/parse.h
@@ -8,16 +8,19 @@
 struct param {
     char key[30];
     char value[20];
+    struct param * next;
 } typedef param;
 
 struct req_data {
-    param * params;
+    param * param_list_head;
+    param * param_list_top;
     char * path;
-    unsigned short length;
 } typedef req_data;
 
 char * create_response(char * msg);
+int push_param(param * pr, req_data * data);
 int parse_query(char * query, req_data * pl);
 int parse_get_request(const char * request, req_data * pl);
+void free_req_data(req_data * data);
 
 #endif
